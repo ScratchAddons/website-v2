8179a775b25e810459004302a620468443173215
Hans5958 <hans5958@outlook.com>
2022-03-10 17:11:14 +0700

    Unobserve when it is not needed

--- static/assets/js/index.js
+++ static/assets/js/index.js
@@ -2,6 +2,7 @@ fetch("https://scratchaddons.com/usercount.json").then(res => res.json()).then((
     const callback = (entries) => {
         if (entries[0].intersectionRatio <= 0) return;
         document.querySelector("#total-users").innerText = count;
+        observer.unobserve(document.querySelector("#total-users"))
     }
     const observer = new IntersectionObserver(callback, {root: null});
     observer.observe(document.querySelector("#total-users"))
