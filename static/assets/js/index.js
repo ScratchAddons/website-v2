16bbfdc1f44c8641a5ad7b899c77b31e56586dfc
Hans5958 <hans5958@outlook.com>
2022-03-10 16:05:48 +0700

    Too many style changes here and there
    Changes, bugs, adjustments, anything in between
    - Fix admonition color on dark theme
    - Fix card columns on different device widths
    - Fix style on sticky docs sidebars
    - Remove underline when hovering docs sidebars, use colors instead
    - Increase padding on index sections
    - Make user count and install buttons side-to-side on wide enough screens
    - Make hero/jumbotron/intro section to cover the whole screen (resolves #12)
    - Make user count animation starts when entering the viewport

--- static/assets/js/index.js
+++ static/assets/js/index.js
@@ -1,7 +1,7 @@
 fetch("https://scratchaddons.com/usercount.json").then(res => res.json()).then(({count}) => {
     const callback = (entries) => {
         if (entries[0].intersectionRatio <= 0) return;
-        setTimeout(() => document.querySelector("#total-users").innerText = count, 500);
+        document.querySelector("#total-users").innerText = count;
     }
     const observer = new IntersectionObserver(callback, {root: null});
     observer.observe(document.querySelector("#total-users"))
