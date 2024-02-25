3fd0b026819ef4823840388013ca6e112221e5ae
WorldLanguages <mundofinky@gmail.com>
2024-02-24 21:07:54 -0300

    Do not end js lines with semicolons

--- static/assets/js/unsupported-browser.js
+++ static/assets/js/unsupported-browser.js
@@ -8,13 +8,13 @@ window.parent.postMessage({msgType: "getVersionInfo"}, "*");
 window.addEventListener("message", (e) => {
     if (e.data.versionInfo) {
         document.getElementById("export-settings").style.display = "block"
-        window.parent.postMessage({msgType: "pageTitleInfo", msgContent: document.title}, "*");
+        window.parent.postMessage({msgType: "pageTitleInfo", msgContent: document.title}, "*")
     }
 });
 
 document.getElementById("export-btn").onclick = () => {
-    window.parent.postMessage({msgType: "exportSettingsAsFile"}, "*");
+    window.parent.postMessage({msgType: "exportSettingsAsFile"}, "*")
 }
 document.getElementById("export-view-btn").onclick = () => {
-    window.parent.postMessage({msgType: "openSettingsAsTab"}, "*");
+    window.parent.postMessage({msgType: "openSettingsAsTab"}, "*")
 }
